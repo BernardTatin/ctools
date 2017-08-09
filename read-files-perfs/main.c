c6338e7d83782e41928c2dfb1a89d85271b660d9
bernard <bernard@localhost.localdomain>
2017-08-10 01:08:28 +0200

    minor changes

--- read-files-perfs/main.c
+++ read-files-perfs/main.c
@@ -68,82 +68,88 @@ static unsigned char buffer[BUFFER_SIZE] ALIGNED(4096); // 1 MiB buffer
 
 static const char *output_file = "/dev/null";
 
-static double now() {
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    return tv.tv_sec + tv.tv_usec / 1000000.;
+static inline double now() {
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  return tv.tv_sec + tv.tv_usec / 1000000.;
 }
 
-static inline int compute_data(unsigned char *data) {
-	int value = 0;
-	int fd = open(output_file, O_WRONLY);
+static int compute_data(unsigned char *data) {
+  int value = 0;
+  int fd = open(output_file, O_WRONLY);
 
-	for (int i=0; i<BUFFER_SIZE; i += 1024) {
-		value += data[i];
-		write(fd, data, 1024);
-	}
-	close(fd);
-	return value;
+  for (int i=0; i<BUFFER_SIZE; i += 1024) {
+    value += data[i];
+    write(fd, data, 1024);
+  }
+  close(fd);
+  return value;
 }
 
-static void test_fread(const char *fileName) {
-    FILE *fp = fopen(fileName, "rb");
+static int test_fread(const char *fileName) {
+  FILE *fp = fopen(fileName, "rb");
+  int value = 0;
 
-    for (int i = 0; i < ITERATIONS; ++i) {
-        fread(buffer, BUFFER_SIZE, 1, fp);
-		compute_data(buffer);
-    }
-    fclose(fp);
+  for (int i = 0; i < ITERATIONS; ++i) {
+    fread(buffer, BUFFER_SIZE, 1, fp);
+    value += compute_data(buffer);
+  }
+  fclose(fp);
+  return value;
 }
 
-static void test_read(const char *fileName) {
-    int fd = open(fileName, O_RDONLY);
+static int test_read(const char *fileName) {
+  int fd = open(fileName, O_RDONLY);
+  int value = 0;
 
-    for (int i = 0; i < ITERATIONS; ++i) {
-        read(fd, buffer, BUFFER_SIZE);
-		compute_data(buffer);
-    }
-    close(fd);
+  for (int i = 0; i < ITERATIONS; ++i) {
+    read(fd, buffer, BUFFER_SIZE);
+    value += compute_data(buffer);
+  }
+  close(fd);
+  return value;
 }
 
-static void test_mmap(const char *fileName) {
-    unsigned char *mmdata;
-    int fd = open(fileName, O_RDONLY);
+static int test_mmap(const char *fileName) {
+  unsigned char *mmdata;
+  int fd = open(fileName, O_RDONLY);
+  int value = 0;
 
-    for (int i = 0; i < ITERATIONS; ++i) {
-        mmdata = mmap(NULL, BUFFER_SIZE, PROT_READ, MAP_PRIVATE, fd, i * BUFFER_SIZE);
-        // But if we don't touch it, it won't be read...
-        // I happen to know I have 4 KiB pages, YMMV
-		compute_data(mmdata);
-        munmap(mmdata, BUFFER_SIZE);
-    }
-    close(fd);
+  for (int i = 0; i < ITERATIONS; ++i) {
+    mmdata = mmap(NULL, BUFFER_SIZE, PROT_READ, MAP_PRIVATE, fd, i * BUFFER_SIZE);
+    // But if we don't touch it, it won't be read...
+    // I happen to know I have 4 KiB pages, YMMV
+    value += compute_data(buffer);
+    munmap(mmdata, BUFFER_SIZE);
+  }
+  close(fd);
+  return value;
 }
 
-static void do_test(void (*fun)(const char *fileName), const char *fileName, const char *testName) {
-    double end_time;
-    double total_time;
-    double start_time = now();
+static void do_test(int (*fun)(const char *fileName), const char *fileName, const char *testName) {
+  double end_time;
+  double total_time;
+  double start_time = now();
 
-    fun(fileName);
+  int value = fun(fileName);
 
-    end_time = now();
-    total_time = end_time - start_time;
+  end_time = now();
+  total_time = end_time - start_time;
 
-    printf("-- %-3s %-5s -- ", COMPILER, testName);
-    printf("It took %7.6f seconds to read %d GiB. That's %10.2f MiB/s.\n", total_time, LOOPS, ITERATIONS / total_time);
+  printf("-- %-3s %-5s -- ", COMPILER, testName, value);
+  printf("It took %7.6f seconds to read %d GiB. That's %10.2f MiB/s.\n", total_time, LOOPS, ITERATIONS / total_time);
 }
 
 int main(int argn, char *argv[]) {
-	char *input_file = "/dev/zero";
+  char *input_file = "/dev/zero";
 
-	if (argn > 1) {
-		input_file = argv[1];
-	}
-    for (int i = 0; i < 10; i++) {
-        do_test(test_fread, input_file, "fread");
-        do_test(test_read, input_file, "read");
-        do_test(test_mmap, input_file, "mmap");
-    }
-    return 0;
+  if (argn > 1) {
+    input_file = argv[1];
+  }
+  for (int i = 0; i < 10; i++) {
+    do_test(test_fread, input_file, "fread");
+    do_test(test_read, input_file, "read");
+    do_test(test_mmap, input_file, "mmap");
+  }
+  return 0;
 }
